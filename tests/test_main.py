d61625d05fd442da6239f4b7d38c3b575210e75f
Robby Reinold <robby@greatexpectations.io>
2023-11-10 14:54:03 -0300

    cover cmd

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,20 @@
+from unittest.mock import patch, Mock
+
 import pytest
 
-test_
+from exfiltrieve.__main__ import execute_cmd
+
+import subprocess
+def test_execute_cmd():
+    with (patch("subprocess.Popen") as popen):
+        input = {'SUPUSERS': {'cmd': "grep -v -E '^#' /etc/passwd | awk -F: '$3 == 0{print $1}'", 'msg': 'Super Users Found:', 'results': []}, 'WHOAMI': {'cmd': 'whoami', 'msg': 'Current User', 'results': []}}
+        expected = {'SUPUSERS': {'cmd': "grep -v -E '^#' /etc/passwd | awk -F: '$3 == 0{print $1}'", 'msg': 'Super Users Found:', 'results': ["1","2"]}, 'WHOAMI': {'cmd': 'whoami', 'msg': 'Current User', 'results': ["3","4"]}}
+
+        process_mock = Mock(name="process")
+        comm = Mock(name="communication",side_effect = [[b"1\n2", None], [b"3\n4", None]])
+
+        process_mock.communicate = comm
+        popen.return_value =process_mock
+
+        actual  = execute_cmd(input)
+        assert actual == expected
